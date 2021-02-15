20287d23137bf8c68b3da7abbe1f79620e6c4c00
Vincent Rousseau <vincent.rousseau.pro@gmail.com>
2021-02-15 10:18:53 +0100

    Create affichage_1.c

---
+++ affichage_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //en-tête de la bibliothèque standard du C
+
+
+int main(){  //déclaration de la fonction main
+    int i,f; //déclaration des variables i et f sont des entiers    
+    int resl; //déclaration de la variable resl
+    
+    i = 0; //initialisation de i par défaut 0
+    f = 0; //initialisation de f
+
+    printf("L'affichage simple' :\n\n"); /* affichage du message et 2 sauts de lignes */
+    printf("saisissez la première valeur: ");  /* instruction à l'utilisateur */
+    scanf("%d", &i); 
+    //scan de la saisie qui doit être une valeur décimale puis 
+    //intitialisation de la valeur l'utilisateur à la variable i
+    printf("\n"); /* saut de ligne */
+    printf("saisissez la deuxième valeur: ");
+    scanf("%d", &f);
+    printf("Votre première valeur = %d\n",i);
+    printf("Votre seconde valeur = %d\n", f);
+    
+    return 0;
+}
